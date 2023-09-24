7ff6ee72ff0028bbb76c0663458d7ed4f21556b2
suzcode <suzetteloomb@gmail.com>
2023-09-24 20:02:30 +1300

    added axios.get

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page updated 17sep2023 </h1>
+    <button @click="fetchData">Ftech Data</button>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+const url = "http://34.111.111.147/microservice1";
+
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      responseData: '',
+    }
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get(url)
+        .then((response) => {
+          this.responseData = response.data;
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    },
+  }
+};
+</script>
 
 <style>
 @media (min-width: 1024px) {
