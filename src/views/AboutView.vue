570fbe87e59f73f82e71c151de606c4cb65e5885
suzcode <suzetteloomb@gmail.com>
2023-11-26 12:12:39 +1300

    changed date

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page updated 26Nov2023 at 12.10pm </h1>
+    <h1>This is an about page updated 26Nov2023 at 12.12pm </h1>
     <br>
     <form @submit="onSubmit" method="POST">
       <input
