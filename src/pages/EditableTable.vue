3331672f05f72d974859c1526c76a1cf75bc06cd
suzcode <suzetteloomb@gmail.com>
2023-12-24 10:41:29 +1300

    changed <pre> to response.data

--- src/pages/EditableTable.vue
+++ src/pages/EditableTable.vue
@@ -4,7 +4,7 @@
     <br />
     <pre>
     <!-- <h1>{{ successMessage }}</h1> -->
-    Response: {{ response }}
+    Response: {{ response.data }}
     <!-- Selected customer: {{ selectedCustomer }}
     Returned customer is: {{ custName }} -->
     <!-- <peopleCom :key="peopleKey">People1: {{ people1 }}</peopleCom> -->
