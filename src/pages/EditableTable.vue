8f94ad6696c71ddb5046526e970b301f21ab4a60
suzcode <suzetteloomb@gmail.com>
2023-12-24 14:46:30 +1300

    added 2.46pm

--- src/pages/EditableTable.vue
+++ src/pages/EditableTable.vue
@@ -4,7 +4,7 @@
     <br />
     <pre>
     <!-- <h1>{{ successMessage }}</h1> -->
-    Response: {{ response.data }}
+    Response 2.46pm: {{ response.data }}
     <!-- Selected customer: {{ selectedCustomer }}
     Returned customer is: {{ custName }} -->
     <!-- <peopleCom :key="peopleKey">People1: {{ people1 }}</peopleCom> -->
