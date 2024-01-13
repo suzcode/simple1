293b35769322a2c525a97ca2c1f9ce9c305e3cec
suzcode <suzetteloomb@gmail.com>
2024-01-14 12:50:35 +1300

    handle pre-existing payload

--- src/pages/EditableTable.vue
+++ src/pages/EditableTable.vue
@@ -211,6 +211,8 @@ function handlePersonUpdate(payload: EmittedValue): void {
   console.log("payloand.id", payload.id);
   console.log("payloand.key", payload.key);
   console.log("payloand.val", Number(payload.val));
+  existingPayload = details.cellChanges;
+  console.log("Pre-existinng Payload", existingPayload);
   // const people = people.value.map((item) => {
   // const container = {};
   // if (item.id == payload.id) {
