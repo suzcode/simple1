a4e1131f24954851b21ef4d200731025bdd4af82
suzcode <suzetteloomb@gmail.com>
2024-01-20 18:30:03 +1300

    added signin with google

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -1,8 +1,41 @@
 <template>
     <div>
         <h1>Profile</h1>
+        <div id="GoogleSignIn" v-if="!isSignedIn">
+            <h3>Google Sign In</h3>
+            <button @click="handleSignInGoogle">Login</button>
     </div>
 </template>
 
 <script>
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const provider = new GoogleAuthProvider
+const auth = getAuth();
+
+const user = '';
+const isSignIn = false;
+
+function handleSignInGoogle() {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log(result);
+            isSignedIn = true;
+        }).catch((error) => {
+            console.log(error);
+        });
+}
+
+function signOut() {
+    signOut(auth)
+        .then(() => {
+            user = '';
+            isSignIn = false;
+        }).catch((error) => {
+            console.log(error);
+        });
+}
+
+
+
 </script>
