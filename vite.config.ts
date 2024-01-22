a8ba4da1bad2385641599cf7504395997f1d8973
suzcode <suzetteloomb@gmail.com>
2024-01-23 06:20:23 +1300

    import fileUrlToPath from url

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath } from 'url'; // Import fileURLToPath from the 'url' module
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
