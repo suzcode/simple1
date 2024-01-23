26c03c0e50a4dd12a42b370c98af18e99c8f95c4
suzcode <suzetteloomb@gmail.com>
2024-01-24 08:42:01 +1300

    changed comment

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/aboutview.vue'
 
-// went away from CamelCase to avoid 404 error 
+// went away from CamelCase to avoid 404 browser error
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
