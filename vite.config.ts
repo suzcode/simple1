4ace9220a35c932b516f095739e4f4a0d37f2cd7
suzcode <suzetteloomb@gmail.com>
2024-02-18 13:42:57 +1300

    updated .htaccess

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-// import { fileURLToPath, URL } from 'node:url'
-// import { fileURLToPath } from 'url'; // Import fileURLToPath from the 'url' module
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/', // Set the base URL to match your Vite build output path
+  base: './', // Set the base URL to match your Vite build output path
   plugins: [
     vue(),
   ],
