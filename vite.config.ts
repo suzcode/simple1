166ae12bcc5e76438c4d4ba5e9d963e2275b3cba
suzcode <suzetteloomb@gmail.com>
2024-02-25 15:12:03 +1300

    changed base to '/' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/dist', 
+  base: '/', 
   // Set the base URL to match your Vite build output path
   plugins: [
     vue(),
