25a50eb43f628784753c32c448d514d85c9517c6
suzcode <suzetteloomb@gmail.com>
2024-03-10 09:42:07 +1300

    updated navbar for /profile and added googlepopup

--- src/pages/UserProfile.vue
+++ src/pages/UserProfile.vue
@@ -5,6 +5,10 @@
             <h3>Google Sign In</h3>
             <button @click="handleSignInGoogle">Login</button>
         </div>
+        <div id="GoogleSignInWithPopup" v-if="!isSignedIn">
+            <h3>Google Sign In with Popup</h3>
+            <button @click="handleSignInGooglePopup">Login with Popup</button>
+        </div>
         <div id="logout" v-if="isSignedIn">
             <button @click="signOut">Logout</button>
         </div>
@@ -18,8 +22,8 @@ import app from "@/firebase/init.js";
 
 const provider = new GoogleAuthProvider();
 const auth = getAuth(app);
-const user = '';
-const isSignIn = false;
+const user = ref(null);
+const isSignedIn = ref(false);
 const result = ref(null);
 const userProfile = ref(null);
 
@@ -51,19 +55,19 @@ function handleSignInGoogle() {
 
 
 // Google popup initiated
-// function handleSignInGoogle() {
-//     signInWithPopup(auth, provider)
-//         .then((result) => {
-//             const credential = GoogleAuthProvider.credentialFromResult(result);
-//             const token = credential.accessToken;
-//             user = result.user.displayName;
-//             console.log(result);
-//             isSignedIn = true;
-//         }).catch((error) => {
-//             const errorMessage = error.message;
-//             console.log(error);
-//         });
-// }
+function handleSignInGooglePopup() {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
+            console.log(result);
+            isSignedIn = true;
+        }).catch((error) => {
+            const errorMessage = error.message;
+            console.log(error);
+        });
+}
 
 function signOut() {
     signOut(auth)
@@ -74,7 +78,4 @@ function signOut() {
             console.log(error);
         });
 }
-
-
-
 </script>
