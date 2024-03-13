69089a0f0d000352c08d983d370a1e5e3b97bd86
suzcode <suzetteloomb@gmail.com>
2024-03-13 22:20:14 +1300

    added code to add user to subcollection

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-      <h1>Sign In</h1>
       <div id="GoogleSignInWithPopup" v-if="!isSignedIn">
         <button @click="signIn">Sign in with Google</button>
       </div>
@@ -13,8 +12,9 @@
   
 <script setup>
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
-import { ref, watchEffect } from "vue";
+import { ref } from "vue";
 import app from "@/firebase/init.js";
+import { getFirestore, collection, doc, setDoc, addDoc, query, where, getDocs } from "firebase/firestore";
 
 const auth = getAuth(app);
 const isSignedIn = ref(false);
@@ -22,21 +22,25 @@ const user = ref(null);
 const uid = ref(null);
 
 const provider = new GoogleAuthProvider();
+const db = getFirestore();
 
 const signIn = () => {
 signInWithPopup(auth, provider)
     .then((result) => {
-    const userCredential = result;
-    const user = userCredential.user;
-    const { displayName, uid } = user;
+        const userCredential = result;
+        const user = userCredential.user;
+        const { displayName, uid } = user;
 
-    console.log("Sign-in provider: Google");
-    console.log("  Name: " + displayName);
-    console.log("  UID: " + uid);
+        console.log("Sign-in provider: Google");
+        console.log("  Name: " + displayName);
+        console.log("  UID: " + uid);
 
-    isSignedIn.value = true;
-    user.value = displayName;
-    uid.value = uid;
+        isSignedIn.value = true;
+        user.value = displayName;
+        uid.value = uid;
+
+        // Add user o Users subcollection
+        addUserToUsersSubcollection(uid, user);
     })
     .catch((error) => {
     console.error("Sign-in error:", error.message);
@@ -44,6 +48,55 @@ signInWithPopup(auth, provider)
     });
 };
   
+const addUserToUsersSubcollection = async (uid, user) => {
+    // Check is subscriber document exists for the current user
+    const subscriberQuery = query(collection (db, "Subscribers"), where("uid", "==", uid));
+    const querySnapshot = await getDocs(subscriberQuery);
+
+    if (querySnapshot.empty) {
+        // Subscriber document doesn't exist, create a default one
+        const defaultSubscriberData = {
+            name: "Default Subscriber",
+        };
+
+        try {
+            const docRef = await addDoc(collection(db, "Subscribers"), defaultSubscriberData);
+            console.log("Default Subscriber document created with ID: ", docRef.id);
+
+            // Now add the user to the Users subcollection under the newly created default Subscriber
+            const userRef = doc(db, "Subscribers", docRef.id, "Users", uid);
+            await setDoc(userRef, {
+                email: user.email,
+                name: user.displayName,
+                status: "user",
+            });
+
+            console.log("User added to Users subcollection under default Subscriber");
+        } catch (error) {
+            console.error("Error creating default Subscriber document:", error);
+        }
+    }
+
+} else {
+    // Subscriber document exists, add user to Users subcollection
+    query.Snapshot.forEach((doc) => {
+        const subscriberId = doc.id;
+        const userRef = doc(db, "Subscribers", subscriberId, "Users", uid);
+
+        setDoc(userRef, {
+            email: user.email,
+            name: user.displayName,
+            status: "user", // default status
+        })
+            .then(() => {
+                console.log("User added to Users subcollection");
+            })
+            .catch((error) => {
+                console.error("Error adding user to Users subcollection:", error);
+            });
+    });
+}
+
 onAuthStateChanged(auth, (currentUser) => {
     if (currentUser) {
         currentUser.providerData.forEach((profile) => {
