f91c864f67bea4a12378a1229e93051bd1151b86
suzcode <suzetteloomb@gmail.com>
2024-03-13 20:03:17 +1300

    changed watchEffect to onAuthStateChanged

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -9,52 +9,56 @@
         <h3 v-if="uid"> uid {{ uid }}</h3>
       </div>
     </div>
-  </template>
+</template>
   
-  <script setup>
-  import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-  import { ref, watchEffect } from "vue";
-  import app from "@/firebase/init.js";
+<script setup>
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { ref, watchEffect } from "vue";
+import app from "@/firebase/init.js";
+
+const auth = getAuth(app);
+const isSignedIn = ref(false);
+const user = ref(null);
+const uid = ref(null);
+
+const provider = new GoogleAuthProvider();
+
+const signIn = () => {
+signInWithPopup(auth, provider)
+    .then((result) => {
+    const userCredential = result;
+    const user = userCredential.user;
+    const { displayName, uid } = user;
+
+    console.log("Sign-in provider: Google");
+    console.log("  Name: " + displayName);
+    console.log("  UID: " + uid);
+
+    isSignedIn.value = true;
+    user.value = displayName;
+    uid.value = uid;
+    })
+    .catch((error) => {
+    console.error("Sign-in error:", error.message);
+    // You may want to display this error message to the user
+    });
+};
   
-  const auth = getAuth(app);
-  const isSignedIn = ref(false);
-  const user = ref(null);
-  const uid = ref(null);
-  
-  const provider = new GoogleAuthProvider();
-  
-  const signIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const userCredential = result;
-        const user = userCredential.user;
-        const { displayName, uid } = user;
-  
-        console.log("Sign-in provider: Google");
-        console.log("  Name: " + displayName);
-        console.log("  UID: " + uid);
-  
-        isSignedIn.value = true;
-        user.value = displayName;
-        uid.value = uid;
-      })
-      .catch((error) => {
-        console.error("Sign-in error:", error.message);
-        // You may want to display this error message to the user
-      });
-  };
-  
-  watchEffect(() => {
-    const currentUser = auth.currentUser;
-    if (currentUser !== null) {
-      currentUser.providerData.forEach((profile) => {
+onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+        currentUser.providerData.forEach((profile) => {
         console.log("Sign-in provider: " + profile.providerId);
         console.log("  Name: " + profile.displayName);
         console.log("  Email: " + profile.email);
         user.value = profile.displayName;
         uid.value = profile.uid;
         isSignedIn.value = true;
-      });
+        });
+    } else {
+        user.value = null;
+        uid.value = null;
+        isSignedIn.value = false;
     }
-  });
-  </script>
+});
+
+</script>
