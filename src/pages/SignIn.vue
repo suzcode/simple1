3b429a19290e322ed1dab3e14b10c6b21d484b3e
suzcode <suzetteloomb@gmail.com>
2024-03-16 09:44:33 +1300

    console log userDetails

--- src/pages/SignIn.vue
+++ src/pages/SignIn.vue
@@ -61,6 +61,7 @@ function addUserToUsersSubcollection(uid, user, displayName) {
         uid: uid,
         displayName: displayName,
     };
+    console.log("userDetails: ", userDetails);
     userDetailsDict["entries"] = userDetails;
     console.log("userDetailsDict", userDetailsDict);
     userDetailsDict_json = JSON.stringify(userDetailsDict);
